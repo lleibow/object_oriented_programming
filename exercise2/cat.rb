a2ae350915736a7b61fdd4e75836c989f970a461
LAURA LEIBOW <lauraleibow@gmail.com>
2017-01-18 16:36:03 -0500

    created two unique instances of Cat class. Added method that calculates hour of the day.

--- exercise2/cat.rb
+++ exercise2/cat.rb
@@ -1,4 +1,6 @@
+#1. Create class Cat
 class Cat
+#2 Created three instance attributes
   attr_accessor :name, :preferred_food, :meal_time
 
   def initialize(name, preferred_food, meal_time)
@@ -7,6 +9,14 @@ class Cat
     @meal_time= meal_time
   end
 
+#4. Add method that returns hour of day
+  def eats_at
+    if @meal_time > 12
+      puts "#{@meal_time-12} pm"
+    else
+      puts "#{@meal_time} am"
+    end
+  end
 end
 
 
@@ -16,3 +26,8 @@ new_cat=Cat.new("Tiny Tuna", "Human Meat", "1 pm")
 puts new_cat.name
 puts new_cat.preferred_food
 puts new_cat.meal_time
+#Two instances of Cat class
+ziggy=Cat.new("Ziggy", "Human Meat", 15)
+marito=Cat.new("Marito", "Orange Peels", 23)
+
+ziggy.eats_at
