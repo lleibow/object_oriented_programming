113e439fd4054b0e1884482d7168b3f865b1ea6f
LAURA LEIBOW <lauraleibow@gmail.com>
2017-01-18 15:24:24 -0500

    I created a method called deposit that gets the amount and adds it to the balance. i was having trouble and then realized I hadn't converted gets to an integer

--- exercise1.rb
+++ exercise1.rb
@@ -5,7 +5,19 @@ class BankAccount
     @balance = balance
     @interest_rates = interest_rates
   end
+
+  def deposit(amount)
+    @balance += amount.to_i
+  end
 end
-bank_account= BankAccount.new("balance","interest")
+
+#create new BankAccount object
+bank_account= BankAccount.new(2000, 0.99)
+
+#display values
 puts bank_account.balance
 puts bank_account.interest_rates
+
+#deposit
+puts "How much would you like to deposit?"
+puts bank_account.deposit(gets.to_i)
