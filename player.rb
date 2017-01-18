c4c3eda5c7706ea905106bd013fdad5a4ea1b6e6
LAURA LEIBOW <lauraleibow@gmail.com>
2017-01-18 17:37:44 -0500

    put starting values for four attributes. Was initially confused but then realized that the attributes won't be based on input values when each instance initializes. The point is it's INITIALized. So it's the values when you start the game. They are always the same

--- player.rb
+++ player.rb
@@ -1,10 +1,10 @@
 class Player
   attr_accessor :gold_coins, :health_points, :lives, :score
 
-  def initialize(gold_coins, health_points, lives, score)
-    @gold_coins = gold_coins
-    @health_points = health_points
-    @lives = lives
-    @score = score
+  def initialize
+    @gold_coins = 0
+    @health_points = 10
+    @lives = 5
+    @score = 0
   end
 end
