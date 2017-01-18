c25875258e9ddf2926caf8b3d9508016ad472f61
LAURA LEIBOW <lauraleibow@gmail.com>
2017-01-18 15:31:21 -0500

    Made withdraw method. Added string balance to deposit and withdraw output so that it's clearer what those values mean.

--- exercise1.rb
+++ exercise1.rb
@@ -1,15 +1,19 @@
 class BankAccount
   attr_accessor :balance, :interest_rates
 
-  def initialize(balance, interest_rates)
-    @balance = balance
-    @interest_rates = interest_rates
-  end
+    def initialize(balance, interest_rates)
+      @balance = balance
+      @interest_rates = interest_rates
+    end
 
-  def deposit(amount)
-    @balance += amount
+      def deposit(amount)
+        @balance += amount
+      end
+
+      def withdraw(amount)
+        @balance -= amount
+      end
   end
-end
 
 #create new BankAccount object
 bank_account= BankAccount.new(2000, 0.99)
@@ -20,4 +24,9 @@ puts bank_account.interest_rates
 
 #deposit
 puts "How much would you like to deposit?"
-puts bank_account.deposit(gets.to_i)
+puts "Your balance is now #{bank_account.deposit(gets.to_i)}"
+
+#withdraw
+
+puts "How much would you like to withdraw"
+puts "Your balance is now #{bank_account.withdraw(gets.to_i)}"
