8355e8a4ca2b29b37444da40e07dde678943b01b
LAURA LEIBOW <lauraleibow@gmail.com>
2017-01-19 01:18:00 -0500

    finished as much as i could

--- player.rb
+++ player.rb
@@ -7,4 +7,30 @@ class Player
     @lives = 5
     @score = 0
   end
+
+  def do_battle
+    @health_points -= 1
+    if @health_points<=0
+      @lives -=1
+      @health_points=10
+    end
+    restart if @lives<=0
+    end
+
+  def restart
+    @gold_coins = 0
+    @health_points = 10
+    @lives = 5
+    @score = 0
+  end
+
 end
+
+player=Player.new
+
+puts player.gold_coins
+
+49.times do
+player.do_battle
+end
+puts "Health After Battle: #{player.health_points}", "Lives After Battle: #{player.lives}"
